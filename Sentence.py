2d2cd44a53aeeb99ab1d96e5ae8565213370fefe
10mmason <10mmason@ashmanorschool.com>
2014-02-05 10:22:05 +0000

    Create Sentence.py

---
+++ Sentence.py
@@ -0,0 +1,6 @@
+start = "Hello, "
+question = input("is it me your looking for? ")
+end = "I can see it in your eyes"
+sentence = start + question + end
+print(sentence)
+#won't actualy make sence but meh
